36782e46dd39daf8203c9b3b75926513b0223edd
Sean Musa <shaunaibek@gmail.com>
2021-07-10 16:44:51 +0100

    rest

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server Running in ${process.env.NODE_ENV} mode @ port ${PORT}`.blue.bold
+    `Server Running in sean mode @ port ${PORT}`.blue.bold
   )
 );
